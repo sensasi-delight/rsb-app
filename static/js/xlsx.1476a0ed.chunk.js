ddf580957c741f1e9ae4255eb153e941694ebcb7
Zain Adam <zainadam.id@gmail.com>
2021-08-29 21:47:48 +0700

    Updates

--- static/js/xlsx.1476a0ed.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprsb-app"]=this["webpackJsonprsb-app"]||[]).push([[2],{612:function(p,n){},699:function(p,n){},700:function(p,n){}}]);
-//# sourceMappingURL=xlsx.1476a0ed.chunk.js.map
---
+++ static/js/xlsx.9ff032e5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprsb-app"]=this["webpackJsonprsb-app"]||[]).push([[2],{618:function(p,n){},705:function(p,n){},706:function(p,n){}}]);
+//# sourceMappingURL=xlsx.9ff032e5.chunk.js.map
